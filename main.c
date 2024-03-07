0851806ef448e12d76d8d86443ce5a20c2560651
IncendioHawk <111070562+IncendioHawk@users.noreply.github.com>
2024-03-07 12:21:44 +0000

    First commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned int getPrimes();
+unsigned int getPhi(n);
+char * getPrivateKey(e, n);
+
+int main() {
+    return 0;
+}
+
+unsigned int getPrimes() {
+    unsigned int primes = {31, 37};
+    return primes;
+}
+
+char * getPrivateKey()
